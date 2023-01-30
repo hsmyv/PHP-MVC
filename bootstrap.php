8f7d49b6e8fb0dbefe4542fb5a9f1d1fbe28a3de
Hasan <hsmusayev@gmail.com>
2023-01-30 15:51:23 +0400

    fix error

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ use Core\Database;
 
 $container = new Container();
 
-$container->bind('Core/Database', function(){
+$container->bind('Core\Database', function(){
     $config = require base_path('config.php');
 
     return new Database($config['database']);
